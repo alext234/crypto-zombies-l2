8e154648fb27ac8846354f1d6e1f03e377785644
alex <alex.tran234@gmail.com>
2018-02-27 21:58:47 +1100

    update indentation

--- src/ZombieFactory.sol
+++ src/ZombieFactory.sol
@@ -6,40 +6,40 @@ contract ZombieFactoryEvents {
 
 contract ZombieFactory is ZombieFactoryEvents {
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+	uint dnaDigits = 16;
+	uint dnaModulus = 10 ** dnaDigits;
 
-    struct Zombie {
-        string name;
-        uint dna;
-    }
+	struct Zombie {
+		string name;
+		uint dna;
+	}
 
-    Zombie[] public zombies;
+	Zombie[] public zombies;
 
-    mapping (uint => address) public zombieToOwner;
-    mapping (address => uint) ownerZombieCount;
+	mapping (uint => address) public zombieToOwner;
+	mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
-        NewZombie(id, _name, _dna);
-    }
+	function _createZombie(string _name, uint _dna) internal {
+		uint id = zombies.push(Zombie(_name, _dna)) - 1;
+		zombieToOwner[id] = msg.sender;
+		ownerZombieCount[msg.sender]++;
+		NewZombie(id, _name, _dna);
+	}
 
-    function _generateRandomDna(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(_str));
-        return rand % dnaModulus;
-    }
+	function _generateRandomDna(string _str) private view returns (uint) {
+		uint rand = uint(keccak256(_str));
+		return rand % dnaModulus;
+	}
 
-    function createRandomZombie(string _name) public {
-        require(ownerZombieCount[msg.sender] == 0);
-        uint randDna = _generateRandomDna(_name);
-        randDna = randDna - randDna % 100;
-        _createZombie(_name, randDna);
-    }
+	function createRandomZombie(string _name) public {
+		require(ownerZombieCount[msg.sender] == 0);
+		uint randDna = _generateRandomDna(_name);
+		randDna = randDna - randDna % 100;
+		_createZombie(_name, randDna);
+	}
 
 	function getZombiesCount() public view returns (uint) {
-	    return zombies.length;
+		return zombies.length;
 	}
 	
 	function getDnaByIndex(uint idx) public view returns (uint) {
